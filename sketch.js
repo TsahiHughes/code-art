23a243823a0b11fe6a6ebaec496495a60381eefa
TsahiHughes <42795459+TsahiHughes@users.noreply.github.com>
2018-11-27 22:42:41 -0800

    update

--- sketch.js
+++ sketch.js
@@ -8,12 +8,12 @@ function preload() {
 img1 = loadImage("traffic.jpg");
 img2 = loadImage("red car.png");
 img3 = loadImage("cyclist.png");
-img4 = loadImage("traffic emoji.png"); 
+img4 = loadImage("traffic emoji.png");
 img5 = loadImage("pandora.png");
 img6 = loadImage("galaxy.jpg");
-}  
+}
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(1500, 1400);
   fill("traffic emoji.png");
 background("traffic.jpg");
 }
@@ -23,14 +23,12 @@ image(img1,400, 400);
   image(img2,mouseY,170);
   image(img3,mouseX, 350);
  image(img4, mouseX, mouseY, 63, 63);
- 
+
 }
  function mousePressed(){
    background(img6);
-   
+
  }
   function mouseReleased(){
     background(img1);
   }
- 
-
